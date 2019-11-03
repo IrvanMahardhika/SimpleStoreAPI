fd3a9658672ccdac27bd3a05f4cf07e619b7922a
IrvanMahardhika <irvan.mahardhika@icloud.com>
2019-11-03 23:37:36 +0700

    intro product detail

--- 2.routers/mailRouters.js
+++
@@ -1,9 +0,0 @@
-var express = require("express")
-var router = express.Router()
-const {mailControler} = require("../1.controlers/index")
-
-router.get("/sendverifyemail", mailControler.sendVerifyEmail)
-router.get("/verifyemail", mailControler.verifyEmail)
-router.get("/sendlinkchangepasswordemail", mailControler.sendLinkChangePasswordEmail)
-
-module.exports = router
