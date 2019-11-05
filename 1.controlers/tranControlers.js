dac9d21c538690beb6fff1dca1a4a172a9f2968c
IrvanMahardhika <irvan.mahardhika@icloud.com>
2019-11-05 21:35:05 +0700

    AddToCartImprove

--- 1.controlers/tranControlers.js
+++ 1.controlers/tranControlers.js
@@ -2,7 +2,51 @@ const db = require("../4.database/index")
 
 module.exports = {
     getCart: (req, res) => {
-        let sql = `select * from cart where userId = ${req.query.userId}`
+        let sql = `select * from cart join products on cart.productId = products.productId left join markdowns on markdowns.productId = products.productId where userId=${req.query.userId}`
+        db.query(sql, (err, result) => {
+            try {
+                if (err) throw err
+                res.send(result)
+            } catch (err) {
+                console.log(err)
+            }
+        })
+    },
+    addToCart: (req, res) => {
+        let sql = `insert into cart values (0,${req.body.userId},${req.body.productId},${req.body.qty},"${req.body.note}")`
+        db.query(sql, (err, result) => {
+            try {
+                if (err) throw err
+                res.send(result)
+            } catch (err) {
+                console.log(err)
+            }
+        })
+    },
+    checkCart: (req, res) => {
+        let sql = `select * from cart where productId = ${req.query.productId} and userId = ${req.query.userId}`
+        db.query(sql, (err, result) => {
+            try {
+                if (err) throw err
+                res.send(result)
+            } catch (err) {
+                console.log(err)
+            }
+        })
+    },
+    updateCart: (req, res) => {
+        let sql = `update cart set qty = ${req.body.qty} where productId = ${req.body.productId} and userId = ${req.body.userId}`
+        db.query(sql, (err, result) => {
+            try {
+                if (err) throw err
+                res.send(result)
+            } catch (err) {
+                console.log(err)
+            }
+        })
+    },
+    deleteCart: (req, res) => {
+        let sql = `delete from cart where cartId = ${req.params.id}`
         db.query(sql, (err, result) => {
             try {
                 if (err) throw err
@@ -12,4 +56,5 @@ module.exports = {
             }
         })
     }
+
 }
--- 2.routers/tranRouters.js
+++ 2.routers/tranRouters.js
@@ -4,5 +4,9 @@ const { verifyToken } = require("../3.helpers/token")
 const { tranControler } = require("../1.controlers/index")
 
 router.get("/getcart", verifyToken, tranControler.getCart)
+router.post("/addtocart", tranControler.addToCart)
+router.get("/checkcart", verifyToken, tranControler.checkCart)
+router.put("/updatecart", tranControler.updateCart)
+router.delete("/deletecart/:id", tranControler.deleteCart)
 
 module.exports = router
