2915ff326df35906c62291bfd419f9899aa236f0
IrvanMahardhika <irvan.mahardhika@icloud.com>
2019-11-08 16:22:35 +0700

    ATC final

--- 1.controlers/tranControlers.js
+++ 1.controlers/tranControlers.js
@@ -35,7 +35,7 @@ module.exports = {
         })
     },
     updateCart: (req, res) => {
-        let sql = `update cart set qty = ${req.body.qty} where productId = ${req.body.productId} and userId = ${req.body.userId}`
+        let sql = `update cart set qty = ${req.body.qty}, note = "${req.body.note}" where productId = ${req.body.productId} and userId = ${req.body.userId}`
         db.query(sql, (err, result) => {
             try {
                 if (err) throw err
@@ -55,6 +55,61 @@ module.exports = {
                 console.log(err)
             }
         })
-    }
+    },
 
+    getCartNonLogin: (req, res) => {
+        let sql = `select cartId, cartnonlogin.userId, cartnonlogin.productId, qty, note, category, brand, name, price, productpic1, start, end, discpercent, discvalue, inventory, measurement, weight, stores.storename, store_cityregency from cartnonlogin join products on cartnonlogin.productId = products.productId join stores on stores.storename = products.storename left join markdowns on markdowns.productId = products.productId where cartnonlogin.userId = ${req.query.userId}`
+        db.query(sql, (err, result) => {
+            try {
+                if (err) throw err
+                res.send(result)
+            } catch (err) {
+                console.log(err)
+            }
+        })
+    },
+    addToCartNonLogin: (req, res) => {
+        let sql = `insert into cartnonlogin values (0,${req.body.userId},${req.body.productId},${req.body.qty},"${req.body.note}")`
+        db.query(sql, (err, result) => {
+            try {
+                if (err) throw err
+                res.send(result)
+            } catch (err) {
+                console.log(err)
+            }
+        })
+    },
+    checkCartNonLogin: (req, res) => {
+        let sql = `select * from cartnonlogin where productId = ${req.query.productId} and userId = ${req.query.userId}`
+        db.query(sql, (err, result) => {
+            try {
+                if (err) throw err
+                res.send(result)
+            } catch (err) {
+                console.log(err)
+            }
+        })
+    },
+    updateCartNonLogin: (req, res) => {
+        let sql = `update cartnonlogin set qty = ${req.body.qty}, note = "${req.body.note}" where productId = ${req.body.productId} and userId = ${req.body.userId}`
+        db.query(sql, (err, result) => {
+            try {
+                if (err) throw err
+                res.send(result)
+            } catch (err) {
+                console.log(err)
+            }
+        })
+    },
+    deleteCartNonLogin: (req, res) => {
+        let sql = `delete from cartnonlogin where cartId = ${req.params.id}`
+        db.query(sql, (err, result) => {
+            try {
+                if (err) throw err
+                res.send(result)
+            } catch (err) {
+                console.log(err)
+            }
+        })
+    }
 }
--- 2.routers/tranRouters.js
+++ 2.routers/tranRouters.js
@@ -9,4 +9,10 @@ router.get("/checkcart", verifyToken, tranControler.checkCart)
 router.put("/updatecart", tranControler.updateCart)
 router.delete("/deletecart/:id", tranControler.deleteCart)
 
+router.get("/getcartnonlogin", tranControler.getCartNonLogin)
+router.post("/addtocartnonlogin", tranControler.addToCartNonLogin)
+router.get("/checkcartnonlogin", tranControler.checkCartNonLogin)
+router.put("/updatecartnonlogin", tranControler.updateCartNonLogin)
+router.delete("/deletecartnonlogin/:id", tranControler.deleteCartNonLogin)
+
 module.exports = router
